de2bd37332149a1070d50907d1b763c9a4359c21
kalilsub <khalilsubaan@gmail.com>
2023-12-22 02:55:10 +0100

    add resource route to for expenses/raw

--- app/routes/expenses_.raw.jsx
+++ app/routes/expenses_.raw.jsx
@@ -1,5 +1,24 @@
-function ExpensesRawPage() {
-  return <div>ExpensesRawPage</div>
-}
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date().toISOString(),
+  },
+]
 
-export default ExpensesRawPage
+export function loader() {
+  return DUMMY_EXPENSES
+}
