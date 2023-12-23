3bbf3011ccb2337ea665158e51131d9698c9aabb
kalilsub <khalilsubaan@gmail.com>
2023-12-23 02:16:18 +0100

    connect /add and /raw to the /expenses index route

--- app/routes/_app.expenses.jsx
+++ app/routes/_app.expenses.jsx
@@ -1,6 +1,7 @@
-import { Outlet } from "@remix-run/react"
+import { Link, Outlet } from "@remix-run/react"
 
 import ExpensesList from "../components/expenses/ExpensesList"
+import { FaDownload, FaPlus } from "react-icons/fa"
 
 const DUMMY_EXPENSES = [
   {
@@ -28,6 +29,16 @@ const ExpensesLayout = () => {
     <>
       <Outlet />
       <main>
+        <section id="expenses-actions">
+          <Link to="add">
+            <FaPlus />
+            <span>Add Expense</span>
+          </Link>
+          <a href="/expenses/raw">
+            <FaDownload />
+            <span>Load Raw Data</span>
+          </a>
+        </section>
         <ExpensesList expenses={DUMMY_EXPENSES} />
       </main>
     </>
