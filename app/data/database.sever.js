ccd796fe3decf9071f90be403a5458eae454a116
kalilsub <khalilsubaan@gmail.com>
2024-01-01 20:44:07 +0100

    set server side validations and form actions

---
+++ app/data/database.sever.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+import { PrismaClient } from "@prisma/client"
+
+/**
+ * @type PrismaClient
+ */
+let prisma
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient()
+  prisma.$connect()
+} else {
+  if (!global.__db) {
+    global.__db = new PrismaClient()
+    global.__db.$connect()
+  }
+  prisma = global.__db
+}
+
+export { prisma }
--- app/routes/_app.expenses.add.jsx
+++ app/routes/_app.expenses.add.jsx
@@ -2,6 +2,20 @@ import ExpenseForm from "../components/expenses/ExpenseForm"
 import Modal from "../components/util/Modal"
 
 import { useNavigate } from "@remix-run/react"
+import { addExpense } from "../data/expenses.server"
+import { redirect } from "@remix-run/node"
+import { validateExpenseInput } from "../data/validation.server"
+
+export async function action({ request }) {
+  const formData = await request.formData()
+  const expenseData = Object.fromEntries(formData)
+  try {
+    validateExpenseInput(expenseData)
+  } catch (error) {}
+
+  await addExpense(expenseData)
+  return redirect("/expenses")
+}
 
 function AddExpensesPage() {
   const navigate = useNavigate()
