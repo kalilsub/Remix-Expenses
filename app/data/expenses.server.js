c64b45daf7f2fe4f29e81678f010c9efcb6833b1
kalilsub <khalilsubaan@gmail.com>
2024-01-07 18:22:12 +0100

    add update and delete functionality

--- app/data/expenses.server.js
+++ app/data/expenses.server.js
@@ -36,3 +36,30 @@ export async function getExpense(id) {
     throw error
   }
 }
+
+export async function updateExpense(id, expenseData) {
+  try {
+    return await prisma.expense.update({
+      where: { id },
+      data: {
+        title: expenseData.title,
+        amount: +expenseData.amount,
+        date: new Date(expenseData.date),
+      },
+    })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export async function deleteExpense(id) {
+  try {
+    return await prisma.expense.delete({
+      where: { id },
+    })
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
--- app/routes/_app.expenses.$id.jsx
+++ app/routes/_app.expenses.$id.jsx
@@ -1,7 +1,30 @@
 import ExpenseForm from "../components/expenses/ExpenseForm"
 import Modal from "../components/util/Modal"
 
-import { useNavigate } from "@remix-run/react"
+import { redirect, useNavigate } from "@remix-run/react"
+import { updateExpense, deleteExpense } from "../data/expenses.server"
+import { validateExpenseInput } from "../data/validation.server"
+
+export const action = async ({ params, request }) => {
+  const expenseId = params.id
+
+  if (request.method === "PATCH") {
+    const formData = await request.formData()
+    const expenseData = Object.fromEntries(formData)
+
+    try {
+      validateExpenseInput(expenseData)
+    } catch (error) {
+      return error
+    }
+
+    await updateExpense(expenseId, expenseData)
+    return redirect("/expenses")
+  } else if (request.method === "DELETE") {
+    await deleteExpense(expenseId)
+    return redirect("/expenses")
+  }
+}
 
 function UpdateExpensesPage() {
   const navigate = useNavigate()
